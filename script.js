b0380968d82d832b7ad7a2a4f2b3ec3e67a49a80
kamigawahorizon <nicolojacobtiu4@yahoo.com>
2023-03-15 10:15:18 +0800

    test

--- script.js
+++ script.js
@@ -23,23 +23,36 @@ function computerPlay() {
 // the game start
 
 function game() {
-    let roundsPlayed = 0;
+    // let roundsPlayed = 0;
     let playerWin = 0;
     let computerWin = 0;
     let gameWinner = "";
 
     // loop through the five rounds, tract the rounds and wins
 
-    while (roundsPlayed <5) {
-        roundsPlayed++;
-        const computerSelection = computerPlay();
-        playerSelection = prompt("Player, please type in your selection (Rock, Paper or Scissor) ")
-        console.log(playRound(capitalize(playerSelection), computerSelection));
-        //console.log(roundsPlayed);
-        console.log("Player win totals " + playerWin);
-        console.log("Computer win totals " + computerWin);
+    // while (roundsPlayed <5) {
+    //     roundsPlayed++;
+    //     const computerSelection = computerPlay();
+    //     playerSelection = prompt("Player, please type in your selection (Rock, Paper or Scissor) ")
+    //     console.log(playRound(capitalize(playerSelection), computerSelection));
+    //     //console.log(roundsPlayed);
+    //     console.log("Player win totals " + playerWin);
+    //     console.log("Computer win totals " + computerWin);
 
         // play the round and determine winner
+
+
+    // Add event listeners for the three buttons
+
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        playerSelection = button.className;
+        const computerSelection = computerPlay();
+        battleWinText.textContent = (playRound(playerSelection, computerSelection));
+        playerWinText.textContent = "Player Win totals: " + playerWin;
+        computerWinText.textContent = "Computer Win titals: " + computerWin;
+    }
+    )
         
 
         function playRound(firstLetterCap, computerSelection){
@@ -105,3 +118,4 @@ function capitalize (playerSelection) {
 game();
 
 
+//git branch test
